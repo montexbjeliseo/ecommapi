8935d095ecc3e8d24768b7f88d4074ef31e09413
montexbjeliseo <secondmtx@gmail.com>
2022-11-05 18:47:09 -0300

    Get categories endpoint

--- src/main/java/com/mtx/ecommerce/mapper/CategoryMapper.java
+++ src/main/java/com/mtx/ecommerce/mapper/CategoryMapper.java
@@ -4,6 +4,7 @@ import com.mtx.ecommerce.dto.request.RegisterCategoryDto;
 import com.mtx.ecommerce.dto.request.UpdateCategoryDto;
 import com.mtx.ecommerce.dto.response.RegisteredCategoryDto;
 import com.mtx.ecommerce.model.Category;
+import java.util.List;
 import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
@@ -15,7 +16,9 @@ public interface CategoryMapper {
     Category toCategory(RegisterCategoryDto dto);
 
     RegisteredCategoryDto toRegisteredDto(Category category);
-    
+
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Category update(UpdateCategoryDto dto, @MappingTarget Category category);
+
+    List<RegisteredCategoryDto> toDtoList(List<Category> categories);
 }
--- src/main/java/com/mtx/ecommerce/service/ICategoryService.java
+++ src/main/java/com/mtx/ecommerce/service/ICategoryService.java
@@ -3,10 +3,13 @@ package com.mtx.ecommerce.service;
 import com.mtx.ecommerce.dto.request.RegisterCategoryDto;
 import com.mtx.ecommerce.dto.request.UpdateCategoryDto;
 import com.mtx.ecommerce.dto.response.RegisteredCategoryDto;
+import java.util.List;
 
 public interface ICategoryService {
 
     RegisteredCategoryDto save(RegisterCategoryDto dto);
 
     RegisteredCategoryDto update(Long id, UpdateCategoryDto dto);
+    
+    List<RegisteredCategoryDto> getAll();
 }
